1a1ecc1d8df5efeec6fbc8843f16fafb18dcea57
mtc <258685818+monty-the-cat@users.noreply.github.com>
2026-02-01 18:13:35 -0800

    Initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import catppuccin from '@catppuccin/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://otherdutiesasassigned.github.io',
+  	base: '/odaa',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Other Duties As Assigned',
+			plugins: [
+        		catppuccin({
+          		// Optional: customize theme flavors and accent colors
+          		dark: { flavor: 'mocha', accent: 'sky' },
+          		light: { flavor: 'latte', accent: 'sky' }
+        })
+      ],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/monty-the-cat/odaa' }],
 			sidebar: [
 				{
 					label: 'Guides',
